4b977909f2fe22cabb9a6298b0f003f62857ed91
Juarez Sampaio <jz@brickabode.com>
2020-07-31 18:45:55 -0300

    Add types to help passing ingredient constraint

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 import numpy as np
-from typing import List
+from typing import List, Union
+from pydantic import BaseModel
 
 class MacroDistribution:
     def __init__(self, protein, carb, fat):
@@ -13,3 +14,76 @@ class MacroDistribution:
     @staticmethod
     def from_dict(row) -> 'MacroDistribution':
         return MacroDistribution(row['Protein'], row['Carb'], row['Fat'])
+
+
+class EqualityConstraint(BaseModel):
+    value: int
+
+
+class MinMaxConstraint(BaseModel):
+    min: int
+    max: int
+
+
+class MinConstraint(BaseModel):
+    min: int
+
+
+class MaxConstraint(BaseModel):
+    max: int
+
+
+Constraint = Union[
+    EqualityConstraint,
+    MinMaxConstraint,
+    MaxConstraint,
+    MinConstraint,
+]
+
+
+class IngredientConstraint(BaseModel):
+    name: str
+    constraint: Constraint
+
+
+class MacroConstraint(BaseModel):
+    protein: EqualityConstraint
+    carb: EqualityConstraint
+    fat: EqualityConstraint
+
+
+class DietConstraints(BaseModel):
+    ingredients: List[IngredientConstraint]
+    macros: MacroConstraint
+
+
+o = {
+    "ingredients": [
+        {
+        "name": "Frango",
+        "constraint": {
+            "value": 180
+            }
+        },
+        {
+        "name": "*",
+        "constraint": {
+            "min": 90
+            }
+        }
+    ],
+    "macros": {
+        "protein": {
+            "value": 90
+        },
+        "carb": {
+            "value": 90
+        },
+        "fat": {
+            "value": 90
+        },
+    }
+}
+
+x = DietConstraints(**o)
+print(x.dict())
